4077db21c29a84aa5ad28deefecaffc2ad8cc13a
возращение Гобнык <ruscobectb228@gmail.com>
2025-04-26 10:07:34 +0500

    added login. Customers in a process

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,27 @@
-export default function Page () {
-    return <p>Customers Page</p>;
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
+import { TableRowSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+export default async function Page() {
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search customers..." />
+      </div>
+
+      <div className="mt-5 flex w-full justify-center">
+        
+      </div>
+    </div>
+  );
 }
