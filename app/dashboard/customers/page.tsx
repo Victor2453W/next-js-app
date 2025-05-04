060f6f97cf28f0b83de40c6e9d4e406b8db3d896
возращение Гобнык <ruscobectb228@gmail.com>
2025-05-05 01:32:43 +0500

    Added customers page. Fixed login. Ended the course

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,12 +1,35 @@
+import { fetchFilteredCustomers } from '@/app/lib/data';
 import { Metadata } from 'next';
- 
+import CustomersTable from '@/app/ui/customers/table';
+import { CustomersTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { lusitana } from '@/app/ui/fonts';
+import  Search from 'app/ui/search';
+
 export const metadata: Metadata = {
   title: 'Customers',
 };
-export default async function Page() {
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
   return (
-    <div className="w-full">
-      
+    <div>
+      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+        Customers
+      </h1>
+      <Search placeholder="Search customers..." />
+      <div className="w-full">
+        <Suspense key={query} fallback={<CustomersTableSkeleton />}>
+          <CustomersTable customers={customers} />
+        </Suspense>
+      </div>
     </div>
   );
 }
--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -59,6 +59,17 @@ export function InvoiceSkeleton() {
   );
 }
 
+export function CustomersTableSkeleton() {
+  return (
+    <div className="mt-6 flow-root">
+      <div className="inline-block min-w-full align-middle">
+        <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export function LatestInvoicesSkeleton() {
   return (
     <div
